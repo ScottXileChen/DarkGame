27a4e629321f7a5e88f66551fde4396c98e33882
ScottXileChen <scottxilechen@gmail.com>
2021-12-23 17:16:42 -0800

    Scott_Chen
    - Added game manager prefab.
    - Added cursor controller.

---
+++ Shimmering/Assets/Scripts/GameManager/CursorController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shimmering
+{
+    public class CursorController : MonoBehaviour
+    {
+        private void Start()
+        {
+            Cursor.visible = false;
+        }
+    }
+}
